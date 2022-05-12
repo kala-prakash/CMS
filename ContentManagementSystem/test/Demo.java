7d3480d27ae2b8f805f456079abb4339a40f7c27
PrakashT <kala.prakash56@gmail.com>
2022-05-12 17:32:29 +0545

    Chore |#182155063| Hashed Password --using SHA-512 algorithm

--- ContentManagementSystem/test/Demo.java
+++ ContentManagementSystem/test/Demo.java
@@ -1,4 +1,10 @@
 
+import static java.lang.Math.random;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -6,27 +12,45 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author kala
  */
 public class Demo {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-         System.out.println("Enter the Semester");
-            System.out.println("[First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth]");
-            String semesterName = input.next();
-            if (!(semesterName.equals("First")||semesterName.equals("Second")||semesterName.equals("Third")||semesterName.equals("Fourth")) && 
-                !(semesterName.equals("Fifth")||semesterName.equals("Sixth")||semesterName.equals("Seventh")||semesterName.equals("Eighth"))){
-                System.out.println("Invalid Option");
-                System.exit(0);
-              }
-            System.out.println("Successful");
-            new Demo();
+    private String password;
+
+    public Demo(String password) {
+        this.password = password;
     }
-}
     
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
   
 
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+       
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the password:");
+    String password = input.next();
+    //Generating Salt
+    //we use the SecureRandom class from java.security
+    SecureRandom random = new SecureRandom();
+    byte[] salt = new byte[16];
+    random.nextBytes (salt);
+    //Then we use the MessageDigest class to configure SHA-512 has function with our salt
+    MessageDigest md = MessageDigest.getInstance("SHA-512");
+    md.update(salt);
+    //And with that added, we can now use the digest method  to generate our hashed password
+    byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+     System.out.println("Password:"+ Arrays.toString(hashedPassword));
+       
+    }
+}
